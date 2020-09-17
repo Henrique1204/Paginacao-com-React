293d554b7d2ffcc18ef81073c4837bfc983399ac
Paulo <pauloh16.pdr@gmail.com>
2020-09-17 17:08:20 -0300

    Updates

---
+++ precache-manifest.2b1782c3e858cd0b53f504c2c37b2e41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a2fd0655c3f31a0eb2a97d81dfcfe0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae998f4221870527b6db",
+    "url": "/static/css/main.bea8d140.chunk.css"
+  },
+  {
+    "revision": "556c806dd568726797d6",
+    "url": "/static/js/2.99be818b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99be818b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae998f4221870527b6db",
+    "url": "/static/js/main.f8044982.chunk.js"
+  },
+  {
+    "revision": "7376cba01ef1f304cd3f",
+    "url": "/static/js/runtime-main.4ee6caaf.js"
+  }
+]);
